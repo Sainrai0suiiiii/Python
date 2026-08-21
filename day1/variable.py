caf458e11589444a5b55e34b269d882792d22752
Sain Rai <sahinkulung@gmail.com>
2026-08-21 22:58:55 +0545

    variables

--- day1/variable.py
+++ day1/variable.py
@@ -4,68 +4,71 @@
 
 # 1. Variables
 name = "Sain"
+name1 = 'sainrai'
+name2 ='kulung'
 age = 25
 height = 5.8
 is_student = True
 
 print(name)
+print(name1,name2)
 print(age)
 print(height)
 print(is_student)
 
 
-# 2. Checking Data Types
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(is_student))
+# # 2. Checking Data Types
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_student))
 
 
-# 3. Basic Arithmetic Operators
-a = 20
-b = 6
+# # 3. Basic Arithmetic Operators
+# a = 20
+# b = 6
 
-print(a + b)   # Addition
-print(a - b)   # Subtraction
-print(a * b)   # Multiplication
-print(a / b)   # Division
-print(a // b)  # Floor division
-print(a % b)   # Modulus
-print(a ** b)  # Exponent
-
-
-# 4. Taking User Input
-user_name = input("Enter your name: ")
-user_age = int(input("Enter your age: "))
-
-print("Hello", user_name)
-print("You are", user_age, "years old.")
+# print(a + b)   # Addition
+# print(a - b)   # Subtraction
+# print(a * b)   # Multiplication
+# print(a / b)   # Division
+# print(a // b)  # Floor division
+# print(a % b)   # Modulus
+# print(a ** b)  # Exponent
 
 
-# 5. Simple Calculation
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+# # 4. Taking User Input
+# user_name = input("Enter your name: ")
+# user_age = int(input("Enter your age: "))
 
-print("Sum:", num1 + num2)
-print("Difference:", num1 - num2)
-print("Product:", num1 * num2)
-print("Division:", num1 / num2)
+# print("Hello", user_name)
+# print("You are", user_age, "years old.")
 
 
-# 6. F-Strings
-name = "Sain"
-age = 25
+# # 5. Simple Calculation
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
 
-print(f"My name is {name} and I am {age} years old.")
+# print("Sum:", num1 + num2)
+# print("Difference:", num1 - num2)
+# print("Product:", num1 * num2)
+# print("Division:", num1 / num2)
 
 
-# 7. Comparison Operators
-x = 10
-y = 20
+# # 6. F-Strings
+# name = "Sain"
+# age = 25
 
-print(x == y)  # Equal
-print(x != y)  # Not equal
-print(x > y)   # Greater than
-print(x < y)   # Less than
-print(x >= y)  # Greater/equal
-print(x <= y)  # Less/equal
+# print(f"My name is {name} and I am {age} years old.")
+
+
+# # 7. Comparison Operators
+# x = 10
+# y = 20
+
+# print(x == y)  # Equal
+# print(x != y)  # Not equal
+# print(x > y)   # Greater than
+# print(x < y)   # Less than
+# print(x >= y)  # Greater/equal
+# print(x <= y)  # Less/equal
